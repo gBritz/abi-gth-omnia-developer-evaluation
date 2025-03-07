17fc36f81240123c0a8db33834eb6d6aea10d2de
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-07 19:44:49 -0300

    feat: create product domain with validations

---
+++ src/Ambev.DeveloperEvaluation.Domain/Validation/ProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation;
+
+internal class ProductValidator : AbstractValidator<Product>
+{
+    public ProductValidator()
+    {
+        RuleFor(product => product.Name)
+            .NotEmpty()
+            .MinimumLength(3).WithMessage("Product name must be at least 3 characters long.")
+            .MaximumLength(200).WithMessage("Product name cannot be longer than 200 characters.");
+
+        RuleFor(product => product.Price)
+            .GreaterThan(0).WithMessage("Product price must be greater than 0.");
+    }
+}
