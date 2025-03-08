d8a2281b3c8676997b3b8c7af45ea54c40e0d713
Guilherme Britz Videira <gbritzv@gmail.com>
2025-03-08 13:03:05 -0300

    feat: associate category name into product in application (#5)
    * feat: implements category repository
    * feat: associate category name into product in application

---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    /// <summary>
+    /// Implementation of <see cref="ICategoryRepository"/> using Entity Framework Core.
+    /// </summary>
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DefaultContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="CategoryRepository"/>.
+        /// </summary>
+        /// <param name="context">The database context</param>
+        public CategoryRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        /// <inheritdoc/>
+        public void Create(Category category)
+        {
+            _context.Categories.Add(category);
+        }
+
+        /// <inheritdoc/>
+        public async Task<Category?> GetByNameAsync(string name, CancellationToken cancellationToken = default)
+        {
+            return await _context.Categories
+                .FirstOrDefaultAsync(u => EF.Functions.ILike(u.Name, $"%{name}%"), cancellationToken);
+        }
+    }
+}
