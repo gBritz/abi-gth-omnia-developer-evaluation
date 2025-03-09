70d9bc1c8a2adf77875d68d2ec0d075f9a19135c
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-09 09:43:36 -0300

    feat: new cart repository to manager sales

---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ICartRepository.cs
@@ -0,0 +1,41 @@
+﻿using Ambev.DeveloperEvaluation.Common.Repositories.Pagination;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    /// <summary>
+    /// Repository interface for <see cref="Cart"/> entity operations.
+    /// </summary>
+    public interface ICartRepository
+    {
+        /// <summary>
+        /// Creates a new cart in the repository.
+        /// </summary>
+        /// <param name="cart">The cart to create</param>
+        void Create(Cart cart);
+
+        /// <summary>
+        /// Deletes a cart from the repository.
+        /// </summary>
+        /// <param name="id">The unique identifier of the cart to delete</param>
+        void Delete(Guid id);
+
+        /// <summary>
+        /// Retrieves a cart by their unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the cart</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The cart if found, null otherwise</returns>
+        Task<Cart?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Retrieves all paginated carts.
+        /// </summary>
+        /// <param name="paging">Info to paginate</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The list of paginated carts</returns>
+        Task<PaginationQueryResult<Cart>> PaginateAsync(
+            PaginationQuery paging,
+            CancellationToken cancellationToken = default);
+    }
+}
