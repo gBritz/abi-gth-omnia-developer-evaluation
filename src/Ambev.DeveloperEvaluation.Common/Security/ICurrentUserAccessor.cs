55486f1d558653476e085812397e3229826b6ff4
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-09 11:47:02 -0300

    feat: new ICurrentUserAccessor interface to access current system user

---
+++ src/Ambev.DeveloperEvaluation.Common/Security/ICurrentUserAccessor.cs
@@ -0,0 +1,13 @@
+﻿namespace Ambev.DeveloperEvaluation.Common.Security;
+
+/// <summary>
+/// Defines way to access current user data.
+/// </summary>
+public interface ICurrentUserAccessor
+{
+    /// <summary>
+    /// Retrieves current user of the system.
+    /// </summary>
+    /// <returns>User of the system.</returns>
+    IUser GetCurrentUser();
+}
