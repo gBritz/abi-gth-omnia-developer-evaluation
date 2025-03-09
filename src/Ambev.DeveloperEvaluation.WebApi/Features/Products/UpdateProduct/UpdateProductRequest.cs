24d64bce1d839e10079a439cacb9220c8ecb25cc
Guilherme Britz Videira <gbritzv@gmail.com>
2025-03-09 19:23:58 -0300

    fix: able to save stock quantity of product (#16)

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Products/UpdateProduct/UpdateProductRequest.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/UpdateProduct/UpdateProductRequest.cs
@@ -33,6 +33,11 @@ public class UpdateProductRequest
     public string Image { get; set; } = default!;
 
     /// <summary>
+    /// Gets the stock quantity
+    /// </summary>
+    public int Quantity { get; set; }
+
+    /// <summary>
     /// Gets the product's rating.
     /// </summary>
     public Rating Rating { get; set; } = default!;
--- src/Ambev.DeveloperEvaluation.WebApi/Features/Products/UpdateProduct/UpdateProductRequestValidator.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Products/UpdateProduct/UpdateProductRequestValidator.cs
@@ -34,16 +34,22 @@ public class UpdateProductRequestValidator : AbstractValidator<UpdateProductRequ
             .MinimumLength(3).WithMessage("Product image must be at least 3 characters long.")
             .MaximumLength(100).WithMessage("Product image cannot be longer than 100 characters.");
 
+        RuleFor(product => product.Quantity)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Product quantity must be greater than or equal to 0.");
+
         RuleFor(product => product.Rating)
             .NotNull();
 
         When(p => p.Rating is not null, () =>
         {
             RuleFor(p => p.Rating.Rate)
-                .GreaterThan(0).WithMessage("Product rating must be greater than 0.");
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("Product rating must be greater than or equal to 0.");
 
             RuleFor(p => p.Rating.Count)
-                .GreaterThan(0).WithMessage("Product rating count must be greater than 0.");
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("Product rating count must be greater than or equal to 0.");
         });
     }
 }
