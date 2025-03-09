90e8ebc21a883d9f113ccc37876081b539a6e3c8
Guilherme Britz Videira <gbritzv@gmail.com>
2025-03-09 09:47:34 -0300

    feat: create cart and cart item domain to manager sales (#10)
    * feat: create cart and cart item entities to manager sales
    * feat: adding stock quantity property into product
    * feat: create método HasOneAsShadow to mapping navigation with shadow foreign key property
    * feat: create database migration to new tables Carts and CartsItems
    * feat: new cart repository to manager sales
    * feat: new method ListByIdsAsync in product repository to retriving products by ids
    * feat: register ICartRepository into container DI

---
+++ src/Ambev.DeveloperEvaluation.Domain/Enums/PurchaseStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Enums;
+
+/// <summary>
+/// Defines a status of purcharse.
+/// </summary>
+public enum PurchaseStatus
+{
+    /// <summary>
+    /// Initial status.
+    /// </summary>
+    Created,
+
+    /// <summary>
+    /// When a order was cancelled.
+    /// </summary>
+    Cancelled,
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/CartRepository.cs
@@ -0,0 +1,63 @@
+﻿using Ambev.DeveloperEvaluation.Common.Repositories.Pagination;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.ORM.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly DefaultContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="CategoryRepository"/>.
+        /// </summary>
+        /// <param name="context">The database context</param>
+        public CartRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        /// <inheritdoc/>
+        public void Create(Cart cart)
+        {
+            _context.Carts.Add(cart);
+        }
+
+        /// <inheritdoc/>
+        public void Delete(Guid id)
+        {
+            Cart cart = new()
+            {
+                Id = id,
+                SaleNumber = default,
+                SoldAt = default,
+                StoreName = default!,
+                CreatedBy = default!,
+                BoughtBy = default!,
+            };
+            _context.Carts.Attach(cart);
+            _context.Carts.Remove(cart);
+        }
+
+        /// <inheritdoc/>
+        public async Task<Cart?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Carts
+                .Include(p => p.Items).ThenInclude(i => i.Product)
+                .FirstOrDefaultAsync(o => o.Id == id, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public async Task<PaginationQueryResult<Cart>> PaginateAsync(
+            PaginationQuery paging,
+            CancellationToken cancellationToken = default)
+        {
+            return await _context.Carts
+                .AsNoTracking()
+                .Include(p => p.Items)
+                .ToPaginateAsync(paging, cancellationToken);
+        }
+    }
+}
