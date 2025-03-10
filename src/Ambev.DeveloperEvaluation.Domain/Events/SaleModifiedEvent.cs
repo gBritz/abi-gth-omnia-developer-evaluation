0febc0cd5b1efc64ebd983151da9933bf2c03c5b
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-10 00:36:09 -0300

    feat: create two new events: sales created and modified

---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleModifiedEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Events;
+
+/// <summary>
+/// Event to notify when sale was modified.
+/// </summary>
+public class SaleModifiedEvent
+{
+    public required Guid CartId { get; init; }
+    public required Guid CustomerId { get; init; }
+    public required string CustomerName { get; init; }
+    public required int TotalProducts { get; init; }
+    public required decimal TotalAmount { get; init; }
+    public required ICollection<SaleProduct> Products { get; init; }
+
+    public record SaleProduct
+    {
+        public required Guid ProductId { get; init; }
+        public required string Title { get; init; }
+        public required decimal Price { get; init; }
+        public required decimal DiscountPercent { get; init; }
+        public required decimal DiscountAmount { get; init; }
+        public required decimal TotalAmount { get; init; }
+    };
+}
