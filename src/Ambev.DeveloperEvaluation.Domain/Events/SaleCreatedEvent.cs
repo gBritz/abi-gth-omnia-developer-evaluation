3ebfdaeb1f1b9adea2ffc8d190e323766cf2b077
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-10 12:06:44 -0300

    refactor: enrichment created and modified sale with more information

--- src/Ambev.DeveloperEvaluation.Domain/Events/SaleCreatedEvent.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleCreatedEvent.cs
@@ -10,6 +10,8 @@ public class SaleCreatedEvent
     public required Guid CartId { get; init; }
     public required Guid CustomerId { get; init; }
     public required string CustomerName { get; init; }
+    public required long SaleNumber { get; init; }
+    public required string StoreName { get; init; }
     public required int TotalProducts { get; init; }
     public required decimal TotalAmount { get; init; }
     public required ICollection<SaleProduct> Products { get; init; }
@@ -21,6 +23,8 @@ public class SaleCreatedEvent
             CartId = cart.Id,
             CustomerId = cart.BoughtById,
             CustomerName = cart.BoughtBy.Username,
+            SaleNumber = cart.SaleNumber,
+            StoreName = cart.StoreName,
             TotalProducts = cart.Items.Count,
             TotalAmount = cart.TotalSaleAmount,
             Products = cart.Items.Select(i => new SaleProduct
