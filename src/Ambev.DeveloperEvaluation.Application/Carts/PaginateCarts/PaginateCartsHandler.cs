f8fb4fbfd87a06f7bc47471d3c623f84c88dd4c6
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-10 19:52:17 -0300

    fix: configure AutoMapper of paginate carts to retrieve fields when paging

--- src/Ambev.DeveloperEvaluation.Application/Carts/PaginateCarts/PaginateCartsHandler.cs
+++ src/Ambev.DeveloperEvaluation.Application/Carts/PaginateCarts/PaginateCartsHandler.cs
@@ -1,4 +1,6 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+﻿using Ambev.DeveloperEvaluation.Application.Common.Mapper;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
 using AutoMapper;
 using MediatR;
 
@@ -33,7 +35,12 @@ public class PaginateCartsHandler : IRequestHandler<PaginateCartsCommand, Pagina
     /// <returns>The product details if found</returns>
     public async Task<PaginateCartsResult> Handle(PaginateCartsCommand request, CancellationToken cancellationToken)
     {
-        var paginationResult = await _cartRepository.PaginateAsync(request, cancellationToken);
+        // note: maps fields from the DTO result to the domain, with names mapped accordingly using AutoMapper.
+        var paging = _mapper.MapFrom(request, [
+            new(typeof(Cart), typeof(CartResult)),
+            new(typeof(CartItem), typeof(CartItemResult))]);
+
+        var paginationResult = await _cartRepository.PaginateAsync(paging, cancellationToken);
         return _mapper.Map<PaginateCartsResult>(paginationResult);
     }
 }
