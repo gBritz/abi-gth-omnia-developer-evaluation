a1d054a4f6c21dd30ba3015adbe4357e64b34a53
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-10 00:39:38 -0300

    feat: install messaging Rebus framework to simulate broker in memory

--- src/Ambev.DeveloperEvaluation.WebApi/Startups/WebApiStartup.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Startups/WebApiStartup.cs
@@ -1,8 +1,13 @@
 ﻿using Ambev.DeveloperEvaluation.Application;
 using Ambev.DeveloperEvaluation.Common.Security;
 using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.Domain.Events;
 using Ambev.DeveloperEvaluation.WebApi.Common;
+using Ambev.DeveloperEvaluation.WebApi.Messaging;
 using MediatR;
+using Rebus.Config;
+using Rebus.Routing.TypeBased;
+using Rebus.Transport.InMem;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Startups
 {
@@ -26,6 +31,24 @@ namespace Ambev.DeveloperEvaluation.WebApi.Startups
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             services.AddScoped<ICurrentUserAccessor, HttpLoggedUserAccessor>();
+        }
+
+        public static void AddRebusMessaging(this IServiceCollection services)
+        {
+            services.AddRebus((configure, provider) => configure
+                .Logging(l => l.Console())
+                .Transport(t => t.UseInMemoryTransport(new InMemNetwork(true), "carts_queue"))
+                .Routing(r => r.TypeBased()
+                    .Map<SaleCreatedEvent>("carts.sale.created")
+                    .Map<SaleModifiedEvent>("carts.sale.modified"))
+                , onCreated: async bus =>
+                {
+                    await bus.Subscribe<SaleCreatedEvent>();
+                    await bus.Subscribe<SaleModifiedEvent>();
+                }
+            );
+            services.AutoRegisterHandlersFromAssemblyOf<Program>();
+            services.AddScoped<IEventNotification, RebusMessageProducer>();
         }
     }
 }
