cc5bf7294941530cd7edb7e71245667239b980a2
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-10 19:52:17 -0300

    fix: configure AutoMapper of paginate carts to retrieve fields when paging

--- src/Ambev.DeveloperEvaluation.Application/Carts/CreateCart/CreateCartProfile.cs
+++ src/Ambev.DeveloperEvaluation.Application/Carts/CreateCart/CreateCartProfile.cs
@@ -18,10 +18,22 @@ public class CreateCartProfile : Profile
             .ForMember(_ => _.UserId, mce => mce.MapFrom(_ => _.BoughtById))
             .ForMember(_ => _.Date, mce => mce.MapFrom(_ => _.SoldAt))
             .ForMember(_ => _.Branch, mce => mce.MapFrom(_ => _.StoreName))
-            .ForMember(_ => _.Cancelled, mce => mce.MapFrom((c, r) => c.PurchaseStatus is PurchaseStatus.Cancelled))
+            .ForMember(_ => _.Cancelled, mce => mce.MapFrom(new CardCancelledResolver(), c => c.PurchaseStatus))
             .ForMember(_ => _.Products, mce => mce.MapFrom(_ => _.Items));
 
         CreateMap<CartItem, CartItemResult>()
-            .ForMember(_ => _.Cancelled, mce => mce.MapFrom((c, r) => c.PurchaseStatus is PurchaseStatus.Cancelled));
+            .ForMember(_ => _.Cancelled, mce => mce.MapFrom(new CardItemCancelledResolver(), i => i.PurchaseStatus));
+    }
+
+    private class CardCancelledResolver : IMemberValueResolver<Cart, CartResult, PurchaseStatus, bool>
+    {
+        public bool Resolve(Cart source,CartResult destination, PurchaseStatus sourceMember, bool destMember, ResolutionContext context) =>
+            source.PurchaseStatus is PurchaseStatus.Cancelled;
+    }
+
+    private class CardItemCancelledResolver : IMemberValueResolver<CartItem, CartItemResult, PurchaseStatus, bool>
+    {
+        public bool Resolve(CartItem source, CartItemResult destination, PurchaseStatus sourceMember, bool destMember, ResolutionContext context) =>
+            source.PurchaseStatus is PurchaseStatus.Cancelled;
     }
 }
