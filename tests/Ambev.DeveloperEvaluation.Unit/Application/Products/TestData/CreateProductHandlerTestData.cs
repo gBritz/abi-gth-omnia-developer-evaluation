3842e22896bbee73bd3d841674308f2da69cc8ee
Guilhere Britz Videira <gbritzv@gmail.com>
2025-03-11 14:37:03 -0300

    test: that products are listed

---
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/Products/TestData/CreateProductHandlerTestData.cs
@@ -0,0 +1,10 @@
+﻿namespace Ambev.DeveloperEvaluation.Unit.Application.Products.TestData;
+
+/// <summary>
+/// Provides methods for generating test data using the Bogus library.
+/// This class centralizes all test data generation to ensure consistency
+/// across test cases and provide both valid and invalid data scenarios.
+/// </summary>
+public static class CreateProductHandlerTestData
+{
+}
